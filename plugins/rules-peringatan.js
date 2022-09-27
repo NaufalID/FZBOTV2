2e1b88f9ec403b3e7e32c07f4f6a970bb7f2397f
NaufalID <88891630+NaufalID@users.noreply.github.com>
2022-09-27 22:58:25 +0700

    Update rules-peringatan.js

--- plugins/rules-peringatan.js
+++ plugins/rules-peringatan.js
@@ -5,7 +5,7 @@ let text = `${htki} RULES ${htka}
 ┃• Telpon/VC Owner = Blok Otomatis
 `
 const templateButtons = [
-    {index: 1, urlButton: {displayText: 'Youtube', url: 'https://youtube.com/channel/UCACHvReRmw2fxgMutPFCBWg'}},
+    {index: 1, urlButton: {displayText: 'Youtube', url: 'https://www.youtube.com/channel/UC1esKQw-DKM5Xa5hF7vqdGA'}},
 ]
 let tm = {
 text: text,
