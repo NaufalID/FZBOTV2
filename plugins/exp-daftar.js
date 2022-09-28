07eed0406052e592ddcf489e6df5bba62178a26c
NaufalID <88891630+NaufalID@users.noreply.github.com>
2022-09-28 08:53:45 +0700

    Update exp-daftar.js

--- plugins/exp-daftar.js
+++ plugins/exp-daftar.js
@@ -60,7 +60,7 @@ let handler = async function (m, { text, usedPrefix, command }) {
 ┗━━━━━━━━━━㉿ 
 ⫹⫺ 
 ⫹⫺`, 
-   footer: `┗ © FangzXD*`, 
+   footer: `┗ © Naufal Muflih*`, 
    title: "", 
    buttonText: "CLICK HERE", 
    sections 
@@ -87,7 +87,7 @@ let handler = async function (m, { text, usedPrefix, command }) {
  ╭━━━━「 *BERHASIL* 」
  ❖ Terima kasih ${name} Sudah mendaftar
 Di Database kami
- ❖ © FANGZ BOT || ALL RESERVED
+ ❖ © Whatsapp || ALL RESERVED
 
  ╭━━━━「 *DATA* 」
  ┊⫹ *Sucsess ☑️* 】Status
